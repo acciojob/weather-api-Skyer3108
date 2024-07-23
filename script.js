e474a981dfcede05f12bdb58d1a5fb319590bfe0
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-07-23 18:01:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 //your JS code here. If required.
+
+const btn=document.getElementById('btn')
+
+const display=document.getElementById('weatherData')
+
+
+
+btn.addEventListener('click',async()=>{
+
+	const apiKey='52cfc6f295dc14cb0448c43506812757'
+
+	const city='London'
+
+	const apiUrt=`https://api.openweathermap.org/data/2.5/weather?q=London&appid=52cfc6f295dc14cb0448c43506812757`
+
+	
+	try{
+
+		console.log('llll')
+
+		const res=await fetch('apiUrt', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+		console.log('llll')
+
+		const data=await res.json()
+		console.log('llll')
+
+		display.textContent=`Current weather in London: ${data}`
+		
+	}catch(err){
+
+		display.textContent=`Error: ${err.message}`
+		
+	}
+	
+})
